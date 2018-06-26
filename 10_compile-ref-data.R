b2df8cfbe73baa527b740924e9ac1d47252743fa
philipp-baumann <philipp.baumann@usys.ethz.ch>
2018-06-26 15:17:15 +0200

    Update the license

---
+++ 10_compile-ref-data.R
@@ -0,0 +1,21 @@
+################################################################################
+## Author: Philipp Baumann (philipp.baumann@usys.ethz.ch)
+## License: GPL-2.0
+## Project: Biophysical, institutional and economic drivers of sustainable 
+##   soil use in yam systems for improved food security in West Africa (YAMSYS)
+## Description: Aggregate chemical reference analysis data prior to
+##   developing spectroscopic reference models for yam elemental content
+##   determination
+################################################################################
+
+## Install all necessary R packages
+## install.packages("devtools", type = "source")
+## devtools::install_github("philipp-baumann/simplerspec")
+
+## Load packages
+pkgs <- c("tidyverse", "simplerspec")
+lapply(pkgs, library, character.only = TRUE)
+
+## Load all plant element data =================================================
+
+
