9a174f041189d0fd4aaaa52e9c3fc4ea8d0ef469
Philipp Baumann <philipp.baumann@usys.ethz.ch>
2019-05-08 13:56:42 +0200

    Install missing packages and load all

---
+++ 00_load-pkgs-run-all.R
@@ -0,0 +1,19 @@
+################################################################################
+## 
+################################################################################
+
+## Install and attach packages
+pkgs <- c("here", "tidyverse", "simplerspec", "future", "doFuture", "A3")
+
+new_pkgs <- pkgs[!(pkgs %in% installed.packages()[, "Package"])]
+
+# Install only new packages
+if (length(new_pkgs)) {
+  if ("devtools" %in% new_pkgs) install.packages("devtools")
+  if ("simplerspec" %in% new_pkgs) {
+    devtools::install_github("philipp-baumann/simplerspec")}
+  install.packages(new_pkgs)
+}
+
+# Load packges
+purrr::iwalk(pkgs, library, character.only = TRUE)
