838322c55e90b378f4d56675abed44209ae415e0
Philipp Baumann <philipp.baumann@usys.ethz.ch>
2019-05-09 11:49:36 +0200

    Remove code because in `R/assess-multimodels.R`

--- 70_evaluate-test.R
+++ 70_evaluate-test.R
@@ -46,51 +46,6 @@ pred_tuber_test <- predict_from_spc(
 predobs_tuber_test <- inner_join(x = pred_tuber_test,
   y = elements_yam_tuber_test)
 
-# Gather all predicted elements in one column
-gather(predobs_tuber_test, key = element_pred, value = pred,
-  pls_tuber_N, pls_tuber_C, pls_tuber_S, pls_tuber_P, pls_tuber_K,
-  pls_tuber_Ca, pls_tuber_Mg, pls_tuber_Fe, pls_tuber_Zn, pls_tuber_Cu)
-
-measure_obs <- c("N", "C", "P", "K", "Ca", "Mg", "Fe", "Zn", "Cu")
-measure_pred <- c("pls_tuber_N", "pls_tuber_C", "pls_tuber_P", "pls_tuber_K",
-  "pls_tuber_Ca", "pls_tuber_Mg", "pls_tuber_Fe", "pls_tuber_Zn",
-  "pls_tuber_Cu")
-
-evaluate_multimodels <- function(data, cols_obs, cols_pred) {
-}
-
-library("data.table")
-
-obs_test_long <- data.table::melt(as.data.table(predobs_tuber_test),
-  measure = measure_obs, variable.name = "variable_obs",
-  value.name = "value_obs", variable.factor = FALSE) %>%
-  select(sample_id, variable_obs, value_obs) %>%
-  mutate(variable = variable_obs) %>%
-  as.tibble()
-
-pred_test_long <- data.table::melt(as.data.table(predobs_tuber_test),
-  measure = measure_pred, variable.name = "variable_pred",
-  value.name = "value_pred", variable.factor = FALSE) %>%
-  select(sample_id, variable_pred, value_pred) %>%
-  as.tibble()
-
-predobs_tuber_test_long <- bind_cols(obs_test_long,
-  pred_test_long)
-
-# Perform model evaluation per variable
-model_eval_train <- predobs_tuber_test_long %>%
-  group_by(variable) %>%
-  nest() %>%
-  mutate(model_eval = map(data,
-    ~ summary_df(.x, x = "value_obs", y = "value_pred"))) %>%
-  unnest(model_eval) %>%
-  select(-data)
-
-## Graphical model evaluation
-predobs_tuber_test_long %>%
-  ggplot(aes(x = value_obs, y = value_pred)) +
-  geom_point() +
-  facet_wrap(~ variable_obs, scales = "free")
 
 ################################################################################
 
